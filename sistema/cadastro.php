b52180fb88b1201fd4983d9a74228bb3384d0a93
camarguin19 <126774745+camarguin19@users.noreply.github.com>
2023-06-22 09:53:21 -0300

    Add files via upload

---
+++ sistema/cadastro.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro do Usuário</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body class= "bg-light">
+    <nav class="navbar bg-primary-subtle">
+        <div class="container-fluid">
+            <a class= "navbar-brand"href="index.php"><img src="recursos/logo-nike-11550726013nlp37aisvx-removebg-preview.png" alt="" width= "100px"></a>
+        </div>
+    </nav>
+
+    <div class= "container text-center mt-5">
+        <h2>Cadastro do Usuário</h2>
+        <p>Preencha as informções abaico e clique no botão 
+            cadastrar para criar seu cadatro.
+        </p>
+        <form action="" method="post">
+            <div class= "row">
+                <div class= "col-md-6">
+                    <input class= "form-control" type="text" name="" id="" placeholder= "Nome">
+                </div>
+                <div class= "col-md-6">
+                    <input class= "form-control" type="text" name="" id="" placeholder= "Sobrenome">
+                </div>
+                
+            </div>
+
+            <div class= "row mt-3"> 
+                <div class= "col-md-10">
+                    <input class="form-control" type="text" name="" id="" placeholder= "Endereço">
+                </div>
+                <div class= "col-md-2">
+                    <input class= "form-control" type="number" name="" id="" placeholder= "Número">
+            </div>
+            <div class= "row mt-3"> 
+                <div class= "col-md-6">
+                    <input class= "form-control" type="number" name="" id="" placeholder= "CPF">
+                </div>  
+                <div class= "col-md-2">
+                    <input class= "form-control" type="datetime" name="" id="" placeholder= "Data de Nascimento">
+                </div> 
+            </div> 
+            <div class= "row mt-3"> 
+                <div class= "col-md-6">
+                    <input class="form-control" type="email" name="" id="" placeholder= "Edereço de email">
+            </div>
+                <div class= "col-md-3">
+                    <input class="form-control" type="password" name="" id="" placeholder= "Senha">
+                </div>
+            
+            <div class= "mt-3">
+            <input class= "btn btn-info btn-lg" type="submit" value="Cadastrar">
+            <a class= "btn btn-danger btn-lg" href="index.php">Já tenho uma conta</a>
+            </div>
+        </form>
+
+    </div>
+    
+    <footer class= "footer py-3 bg-dark fixed-bottom">
+        <div class= "container text-center">
+            <p class= "text-light">Todos os direitos reservado - 2023</p>
+        </div>
+    </footer>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+<?php
+
+
+?>
---
+++ sistema/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body class= "bg-dark-subtle">
+    <div class="container text-center mt-3 mb-1">
+        <img src="recursos/logo-nike-11550726013nlp37aisvx-removebg-preview.png" alt="" width="400px">
+    </div>    
+    
+    <div class="container text-center mb-5">
+        <h2 class="text-light">Tela de login</h2>
+    </div>
+
+    <div class="container text-center mt-5">
+        <form action="" method="post">
+            <label class="form-label" for="usuario">Usuário:</label>
+            <input class="form-control mb-2" type="email" name="" id="usuario ">
+
+            <label class= "form-label" for="senha">Senha:</label>
+            <input class= "form-control mb-2" type="password" name="" id="senha">
+
+            <input class= "btn btn-info btn-lg" type="submit" value="Login">
+            <a class= "btn btn-light btn-lg" href="cadastro.php">Cadastrar</a>
+        </form>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
